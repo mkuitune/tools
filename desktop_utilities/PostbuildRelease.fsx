e1f9ea3e6b19fac74f967af6224624df2ca97401
MikkoK <invalid@ddress>
2014-06-10 22:44:32 +0300

    First desktop tool commit

---
+++ desktop_utilities/PostbuildRelease.fsx
@@ -0,0 +1,6 @@
+﻿#load "DesktopUtilities.fs"
+
+open DUP
+
+// Include C:\bin in your path.
+DUP.File.Copy(@"bin\Release\DesktopUtilities.dll", @"C:\bin\DesktopUtilities.dll")
---
+++ desktop_utilities/Script.fsx
@@ -0,0 +1,12 @@
+﻿
+#load "DesktopUtilities.fs"
+
+open DUP
+
+type Dir = DUP.Directory
+
+let srcdir = @"D:\tmp\from"
+let targetdir = @"D:\tmp\to"
+Dir.Copy(srcdir, targetdir)
+Dir.DeleteContents(targetdir)
+
